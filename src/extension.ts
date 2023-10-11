25987624a3998c7ec58de555ae854ef2b0a0f3dd
Petar Garžina <petar.garzina@gmail.com>
2023-10-11 09:11:52 +0200

    Add pendo-ignore

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(
-  context: vscode.ExtensionContext
-) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "pendo-paste" is now active!'
-  );
+export function activate(context: vscode.ExtensionContext) {
+  let pendoClass = vscode.commands.registerCommand("pendo-paste.pendoClass", function () {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+    const uniqueString = makeid(20);
+    const pendoClass = `pendo-${uniqueString}`;
 
+    editor.edit((editBuilder) => {
+      const cursor = editor.selection;
+      editBuilder.insert(cursor.active, pendoClass);
+    });
+  });
+
+  let pendoIgnore = vscode.commands.registerCommand("pendo-paste.pendoIgnore", function () {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const pendoIgnore = `pendo-ignore`;
+
+    editor.edit((editBuilder) => {
+      const cursor = editor.selection;
+      editBuilder.insert(cursor.active, pendoIgnore);
+    });
+  });
+
+  /**
+   * TODO: Find all duplicate pendo classes
+   */
+  // let pendoDuplicates
+
+  /** @see https://stackoverflow.com/a/1349426 */
   function makeid(length: number) {
     let result = "";
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < length) {
-      result += characters.charAt(
-        Math.floor(
-          Math.random() * charactersLength
-        )
-      );
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
       counter += 1;
     }
     return result;
   }
-  let disposable =
-    vscode.commands.registerCommand(
-      "pendo-paste.pendoPaste",
-      function () {
-        // Get the active text editor
-        const editor =
-          vscode.window.activeTextEditor;
 
-        if (editor) {
-          const uniqueString = makeid(20);
-          const pendoClass = `pendo-${uniqueString}`;
-
-          editor.edit((editBuilder) => {
-            const cursor = editor.selection;
-            editBuilder.insert(
-              cursor.active,
-              pendoClass
-            );
-          });
-        }
-      }
-    );
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(pendoClass, pendoIgnore);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
